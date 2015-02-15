643942174cc589d7eefb1bddcff1cacfa50fe615
karmazin <karmazzin@ya.ru>
2015-02-15 22:59:23 +0300

    Move template

--- src/modules/dash/dash.js
+++ src/modules/dash/dash.js
@@ -7,7 +7,7 @@ angular.module('metrikangular.dash', [])
             url: "/main",
             controllerAs: 'main',
             controller: "metrikangular.dash.main",
-            templateUrl: "src/modules/dash/view/main.html",
+            templateUrl: "theme/templates/dash/main.html",
             resolve: {
 
             },
